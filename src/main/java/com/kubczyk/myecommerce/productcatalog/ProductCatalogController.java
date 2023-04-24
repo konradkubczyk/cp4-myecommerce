7d63b229132c1a652d4767d8e3c42efdf7f907c0
Konrad Kubczyk <konrad@kubczyk.com>
2023-04-24 17:21:16 +0200

    Introduce UI

--- src/main/java/com/kubczyk/myecommerce/productcatalog/ProductCatalogController.java
+++ src/main/java/com/kubczyk/myecommerce/productcatalog/ProductCatalogController.java
@@ -3,7 +3,6 @@ package com.kubczyk.myecommerce.productcatalog;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
